111a717b00c4bfa1bca18468b957f39b80f946be
Policaika <serezapovisok@gmail.com>
2026-03-15 18:03:44 +0300

    Added a new version of print Hello world with target on person

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-cout << "Hello world" << endl;
+string name;
+cout << "Введите имя пользователя: ";
+getline(cin,name);
+cout << "Hello world from @" << name << endl;
 return 0;
 }
+
